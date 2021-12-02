32868768fee18440906ab1e9ff2d085643fa2cf4
AdamShepherd19 <shepherd.adam.2000@gmail.com>
2021-12-02 16:01:03 +0000

    Stage 2 complete and struct created

--- day_2/day_2.cpp
+++ day_2/day_2.cpp
@@ -3,14 +3,83 @@
 
 using namespace std;
 
-int main(){
-    vector<string> data = readFile<string>("day_2_input.txt");
+struct instruction {
+    string instruction;
+    int amount;
+};
 
-    for (int i = 0; i < data.size(); i++)
+int stageOne(vector<instruction> analysedData)
+{
+    int horizontal = 0;
+    int depth = 0;
+
+    for (int x = 0; x < analysedData.size(); x++)
     {
-        cout << data[i] << endl;
+        if (analysedData[x].instruction.compare("forward") == 0)
+        {
+            horizontal += analysedData[x].amount;
+        } 
+        else if (analysedData[x].instruction.compare("down") == 0)
+        {
+            depth += analysedData[x].amount;
+        } 
+        else if (analysedData[x].instruction.compare("up") == 0)
+        {
+            depth -= analysedData[x].amount;
+        }
     }
     
+    return horizontal * depth;
+}
+
+int stageTwo(vector<instruction> analysedData)
+{
+    int horizontal = 0;
+    int depth = 0;
+    int aim = 0;
+
+    for (int x = 0; x < analysedData.size(); x++)
+    {
+        if (analysedData[x].instruction.compare("forward") == 0)
+        {
+            horizontal += analysedData[x].amount;
+            depth += aim * analysedData[x].amount;
+        } 
+        else if (analysedData[x].instruction.compare("down") == 0)
+        {
+            aim += analysedData[x].amount;
+        } 
+        else if (analysedData[x].instruction.compare("up") == 0)
+        {
+            aim -= analysedData[x].amount;
+        }
+
+    }
+
+    return horizontal * depth;
+}
+
+vector<instruction> analyseData(vector<string> data)
+{
+    vector<instruction> analysedData;
+
+    for (int x = 0; x < data.size(); x++)
+    {
+        instruction temp;
+        temp.instruction = data[x].substr(0, data[x].find(" "));
+        temp.amount = stoi(data[x].substr(data[x].find(" ") + 1, data[x].size()));
+        analysedData.push_back(temp);
+    }
+    
+    return analysedData;
+}
+
+int main(){
+    vector<string> data = readFile<string>("day_2_input.txt");
+    vector<instruction> analysedData = analyseData(data);
+    
+    cout << "Stage One: " << stageOne(analysedData) << endl;
+    cout << "Stage Two: " << stageTwo(analysedData) << endl;
 
     return 0;
 }
